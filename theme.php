2cb60b57b87f5e601d63ef3639cc902325ffc4ee
Robert Isoski <r@robiso.com>
2019-11-10 19:41:23 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -7,20 +7,20 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
-        <meta name="description" content="<?= $Wcms->page('description') ?>">
-        <meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
+        	<title><?= $Wcms->get('config', 'siteTitle') ?> - <?= $Wcms->page('title') ?></title>
+        	<meta name="description" content="<?= $Wcms->page('description') ?>">
+        	<meta name="keywords" content="<?= $Wcms->page('keywords') ?>">
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
-        <?= $Wcms->css() ?>
+       	 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        	<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
+        	<?= $Wcms->css() ?>
 
 	</head>
 	
 	<body>
-        <?= $Wcms->alerts() ?>
-        <?= $Wcms->settings() ?>
+        	<?= $Wcms->settings() ?>
+		<?= $Wcms->alerts() ?>
 
 		<nav class="navbar navbar-default">
 			<div class="container">
