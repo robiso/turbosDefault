28f2a1b2f69d801af67413d906fba98d7a6bb9b1
Robert Isoski <r@robiso.com>
2021-12-31 00:18:12 +0100

    Update theme.php

--- theme.php
+++ theme.php
@@ -13,10 +13,14 @@
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
        	 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        	<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
-        	<?= $Wcms->css() ?>
 
-	</head>
+		<!-- Admin CSS -->
+		<?= $Wcms->css() ?>
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>">
+	    </head>
+
 	
 	<body>
         	<?= $Wcms->settings() ?>
